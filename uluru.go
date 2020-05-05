40aef24926e9ec60b9624acd2bad6b37510b263a
Camilo Acuña <camilo.aku@gmail.com>
2020-05-05 05:49:05 -0400

    update main uluru file

--- uluru.go
+++ uluru.go
@@ -1,1 +1,14 @@
 package uluru
+
+// Response ...
+type Response struct {
+	Data  interface{} `json:"data,omitempty"`
+	Meta  interface{} `json:"meta,omitempty"`
+	Error interface{} `json:"error,omitempty"`
+}
+
+// ClientOptions ...
+type ClientOptions struct {
+	Token       string
+	Environment string
+}
