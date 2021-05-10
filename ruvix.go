e7ffce83857511d0fa0dc16be720e1482b39ae4d
Camilo Acuña <camilo.aku@gmail.com>
2021-05-10 05:53:02 -0400

    add main file

---
+++ ruvix.go
@@ -0,0 +1,14 @@
+package ruvixapi
+
+// Response ...
+type Response struct {
+	Data  interface{} `json:"data,omitempty"`
+	Meta  interface{} `json:"meta,omitempty"`
+	Error interface{} `json:"error,omitempty"`
+}
+
+// ClientOptions ...
+type ClientOptions struct {
+	Token       string
+	Environment string
+}
